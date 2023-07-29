8284aab127df0d144c2852d83cd809904acf76a5
Burak Boduroğlu <burakboduroglu0@gmail.com>
2023-07-29 12:12:43 +0300

    Initial Express Setup

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+// load env vars
+dotenv.config({ path: "./config/config.env" });
+
+// create app
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
